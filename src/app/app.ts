fd3b7c2bd13a1e12fe455103c07b6cf03a68982c
Stiven <johnstiven40@gmail.com>
2025-08-13 12:32:20 -0500

    f

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,4 +8,24 @@ import { Component, signal } from '@angular/core';
 })
 export class App {
   protected readonly title = signal('my-app');
+  nombres: string[] = [];
+  nums: number[] = [];
+
+  nombre(){
+    for(var i = 0; i < 5; i++){
+      const nombre = prompt("Ingrese el nombre " + i);
+      if (nombre != null){
+        this.nombres.push(nombre);
+      }
+    }
+  }
+
+  par(){
+    for(var i = 0; i < 10; i++){
+      const numero = Number(prompt("Ingrese un numero, contador: " + i));
+      if (numero != null && numero % 2 == 0){
+        this.nums.push(numero);
+      }
+    }
+  }
 }
